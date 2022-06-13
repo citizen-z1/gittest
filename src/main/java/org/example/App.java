1e4dc9c105765bf4c6b6683ce0294eb0e621d56e
juran <juranwh01@163.com>
2022-06-13 15:14:38 +0800

    second commit

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -8,7 +8,7 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println(1);
+        System.out.println("version2");
         System.out.println( "Hello World!" );
     }
 }
